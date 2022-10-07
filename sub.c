45fb35d6df08a49559af6e16a96f47e93ea238f3
bek2518 <bereketb20@hotmail.com>
2022-10-07 17:58:52 +0300

    Minor edit

--- sub.c
+++ sub.c
@@ -4,21 +4,21 @@
  * sub - subtracts the top two nodes of stack
  * @stack: pointer to the stack
  * @line_number: line number
- * Return: pointer to new stack 
+ * Return: pointer to new stack
  */
 void sub(stack_t **stack, unsigned int line_number)
 {
-        int difference = 0;
-        stack_t *head = *stack;
+	int difference = 0;
+	stack_t *head = *stack;
 
-        if (!head || !head->next)
-        {
-                fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-                free_stack(stack);
-                exit(EXIT_FAILURE);
-        }
-        difference = (head->next->n) + (head->n);
-        head->next->n = difference;
-        pop(stack, 0);
+	if (!head || !head->next)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	difference = (head->next->n) - (head->n);
+	head->next->n = difference;
+	pop(stack, 0);
 }
 
