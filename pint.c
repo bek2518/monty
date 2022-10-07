09496cfb68d493104e5d7b3a90cb49f6a9fdb386
bek2518 <bereketb20@hotmail.com>
2022-10-07 17:09:22 +0300

    pint modification, betty style and docs

--- pint.c
+++ pint.c
@@ -3,8 +3,7 @@
 /**
  * pint - Prints the integer of the head
  * @stack: pointer to the stack
- * @line number: line number
- * Return: pointer to the stack
+ * @line_number: line number
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
@@ -17,6 +16,11 @@ void pint(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	else
-		printf("%d\n", temp->n);
+	while (temp)
+	{
+		if (temp->prev == NULL)
+			break;
+		temp = temp->prev;
+	}
+	printf("%d\n", temp->n);
 }
