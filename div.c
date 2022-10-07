8eb375d8968903e0fb9e4850be87f271d6f24f0f
bek2518 <bereketb20@hotmail.com>
2022-10-07 18:03:52 +0300

    Minor edit

--- div.c
+++ div.c
@@ -3,29 +3,30 @@
 /**
  * div_op - This function divides the second top nodes of the stack by the top
  * @stack: The stack list
- * @line_num: The line number of the file
+ * @line_number: The line number of the file
  * Return: The edited stack
  */
 
-void div_op(stack_t **stack, unsigned int line_num)
+void div_op(stack_t **stack, unsigned int line_number)
 {
-	int result = 0;
+	int result;
 	stack_t *head = *stack;
 
 	if (!head || !head->next)
 	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", line_num);
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
 		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
 
 	if (head->n == 0)
 	{
-		fprintf(stderr, "L%d: division by zero\n", line_num);
+		fprintf(stderr, "L%d: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	result = (head->next->n) / (head->n);
 	head->next->n = result;
+	head->prev = NULL;
 	pop(stack, 0);
 }
