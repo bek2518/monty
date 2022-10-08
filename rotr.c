e57a6e20587cb7a987cccfe3db59a348d3c35430
bek2518 <bereketb20@hotmail.com>
2022-10-08 13:14:03 +0300

    rotr edit

--- rotr.c
+++ rotr.c
@@ -8,6 +8,22 @@
  */
 void rotr(stack_t **stack, unsigned int line_number __attribute__((unused)))
 {
-	if (*stack)
-		*stack = (*stack)->prev;
+	stack_t *last, *temp;
+
+	if (stack == NULL || (*stack) == NULL || ((*stack)->next) == NULL)
+		return;
+
+	temp = *stack;
+	last = temp;
+
+	while (last->next)
+	{
+		last = last->next;
+	}
+
+	last->prev->next = NULL;
+	last->prev = NULL;
+	temp->prev = last;
+	last->next = temp;
+	*stack = last;
 }
