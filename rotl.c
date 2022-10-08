8c817b3b62978c09ab2d1b7f1b714ce6d7a4f18b
bek2518 <bereketb20@hotmail.com>
2022-10-08 13:12:27 +0300

    remove old code

--- rotl.c
+++ rotl.c
@@ -7,27 +7,6 @@
  * Return: pointer to stack
  */
 void rotl(stack_t **stack, unsigned int line_number __attribute__((unused)))
-/**{
-	stack_t *temp;
-	int i, j;
-
-	if (stack == NULL || *stack == NULL)
-		nop(stack, line_number);
-
-	temp = *stack;
-	i = (*stack)->n;
-
-	while (temp)
-	{
-		if (temp->next == NULL)
-			break;
-		temp = temp->next;
-	}
-	j = temp->n;
-	(*stack)->n = j;
-	temp->n = i;
-
-}*/
 {
 	stack_t *last, *temp;
 
